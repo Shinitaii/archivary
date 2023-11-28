91e0ffc9f4275983ea0884455de339d1d4c3f990
Arthur Artugue <artuguearthur@gmail.com>
2023-11-28 18:01:07 +0800

    added class to test github

---
+++ Archivary/ClassToTestGit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivary
+{
+    internal class ClassToTestGit
+    {
+        //WAG NIYOO PANSININ TO
+    }
+}
